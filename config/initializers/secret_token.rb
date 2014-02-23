ab5e1dfce03108cffeca5593b1bc3ef76ceade8c
peeyush <peeyushagarwal1994@gmail.com>
2014-02-23 10:35:00 +0530

    Created a rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Eduvate::Application.config.secret_key_base = 'bbc53818ac4c66686678162fe49a8aa3e5c9ae7d04e2c3ede1bcbd6fdc53c69664868ba04a3e7a512a8d6442bf6ce6269f54d034a08bc1cb7d35f2dcded9aa6e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Eduvate::Application.config.session_store :cookie_store, key: '_eduvate_session'
